9f01bb0df7f39967b63ac66c5e7e1020b790ea60
KubilayGurel <kubilay.kubi99@gmail.com>
2024-09-28 01:52:54 +0300

    firsta

--- app/src/main/java/com/kubilaygurel/artbookfragment/view/GaleryFragment.kt
+++ app/src/main/java/com/kubilaygurel/artbookfragment/view/GaleryFragment.kt
@@ -4,11 +4,13 @@ import android.app.Activity.RESULT_OK
 import android.app.ProgressDialog.show
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.util.Size
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,16 +29,15 @@ import com.kubilaygurel.artbookfragment.databinding.FragmentGaleryBinding
 import com.kubilaygurel.artbookfragment.model.ArtList
 import com.kubilaygurel.artbookfragment.model.ArtlistDataBase
 import com.kubilaygurel.artbookfragment.roomdb.artListDao
+import java.io.ByteArrayOutputStream
 
 
 class GaleryFragment : Fragment() {
 
     private lateinit var db: ArtlistDataBase
     private lateinit var artlistDao: artListDao
-    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
-    private lateinit var permissionLaunncher :  ActivityResultLauncher<String>
-    var selectedBitmap = null
-
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLaunncher: ActivityResultLauncher<String>
     private var _binding: FragmentGaleryBinding? = null
     private val binding get() = _binding!!
 
@@ -57,12 +58,13 @@ class GaleryFragment : Fragment() {
         binding.imageView.setOnClickListener {
             artlistDao.getAll()
         }
-        binding.imageView.setOnClickListener{
+        binding.imageView.setOnClickListener {
             selectImage(view)
         }
     }
 
     private fun save() {
+
         val artlist = ArtList(
             binding.artistNameText.text.toString(),
             binding.artNameText.text.toString(),
@@ -70,6 +72,26 @@ class GaleryFragment : Fragment() {
         )
         artlistDao.insert(artlist).subscribe()
         findNavController().navigate(R.id.action_galeryFragment_to_homeFragment)
+    }
+
+    private fun makeSmallerBitmap(image: Bitmap, maximumSize: Int): Bitmap {
+
+        var width = image.width
+        var height = image.height
+        val bitmapRatio: Double = width.toDouble() / height.toDouble()
+
+        if (bitmapRatio > 1) {
+
+            width = maximumSize
+            val scaledHeight = width / bitmapRatio
+            height = scaledHeight.toInt()
+
+        } else {
+            width = maximumSize
+            val scaledWith = height * bitmapRatio
+            width = scaledWith.toInt()
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true)
     }
 
     override fun onCreateView(
@@ -82,74 +104,100 @@ class GaleryFragment : Fragment() {
     }
 
 
+    fun selectImage(view: View) {
 
-    fun selectImage(view: View){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
 
-            if (ContextCompat.checkSelfPermission(requireContext(),android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        android.Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
+                    Snackbar.make(view, "Galeri için izin ver!", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("İZİN VER", View.OnClickListener {
+                            permissionLaunncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
 
-                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.READ_MEDIA_IMAGES)){
-                    Snackbar.make(view,"Galeri için izin ver!",Snackbar.LENGTH_INDEFINITE).setAction("İZİN VER",View.OnClickListener {
-                        permissionLaunncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
-                    }).show()
-
-                }else{
+                } else {
                     permissionLaunncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
 
 
                 }
 
-            }else{
-                val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            } else {
+                val intentToGalery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 activityResultLauncher.launch(intentToGalery)
 
             }
 
-        }else{
-            if (ContextCompat.checkSelfPermission(requireContext(),android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
 
-                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.READ_EXTERNAL_STORAGE)){
-                    Snackbar.make(view,"Galeri için izin ver!",Snackbar.LENGTH_INDEFINITE).setAction("İZİN VER",View.OnClickListener {
-                        permissionLaunncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                    }).show()
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    Snackbar.make(view, "Galeri için izin ver!", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("İZİN VER", View.OnClickListener {
+                            permissionLaunncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }).show()
 
-                }else{
+                } else {
                     permissionLaunncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
                 }
 
-            }else{
-                val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            } else {
+                val intentToGalery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 activityResultLauncher.launch(intentToGalery)
 
             }
 
         }
 
-        }
+    }
 
-        private fun registerLauncher(){
-            activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-                if (result.resultCode == RESULT_OK){
+    private fun registerLauncher() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
                     val intentFromResult = result.data
-                    if (intentFromResult != null){
-                       val imageData = intentFromResult.data
+                    if (intentFromResult != null) {
+                        val imageData = intentFromResult.data
 
 
 
                         try {
-                            if (Build.VERSION.SDK_INT >= 28){
-                            val source = ImageDecoder.createSource(requireContext().contentResolver,imageData!!)
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                val source = ImageDecoder.createSource(
+                                    requireContext().contentResolver,
+                                    imageData!!
+                                )
                                 ImageDecoder.decodeBitmap(source)
-                            binding.imageView.setImageBitmap(selectedBitmap)
-                            }else{
-                                MediaStore.Images.Media.getBitmap(requireContext().contentResolver,imageData)
+                                binding.imageView.setImageBitmap(imageData)
+                            } else {
+                                MediaStore.Images.Media.getBitmap(
+                                    requireContext().contentResolver,
+                                    imageData
+                                )
                             }
-                            binding.imageView.setImageBitmap(selectedBitmap)
+                            binding.imageView.setImageBitmap(imageData)
 
 
-                        }catch (e: Exception){
+                        } catch (e: Exception) {
                             e.printStackTrace()
                         }
 
@@ -158,17 +206,19 @@ class GaleryFragment : Fragment() {
                 }
 
             }
-            permissionLaunncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+        permissionLaunncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
 
-                if (result){
+                if (result) {
 
-                    val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    val intentToGalery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                     activityResultLauncher.launch(intentToGalery)
 
 
-                }else{
+                } else {
                     Toast.makeText(requireContext(), "İZİN GEREKLİ", Toast.LENGTH_LONG).show()
                 }
             }
-        }
     }
+}
